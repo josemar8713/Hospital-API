e9c895b1fb4193510ab3f1e3b2f5498784641be8
jose martin <josemar8713@gmail.com>
2023-12-31 00:50:18 -0600

    desabilitacion de csrf e implementacion de cors

--- src/main/java/com/Usuarios/web/config/SecurityConfig.java
+++ src/main/java/com/Usuarios/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.Usuarios.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,9 +10,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
         http
+                .csrf().disable()
+                .cors().and()
                 .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET,"/api/patients/*").permitAll() //permite todo lo de get sin contraseña
                 .anyRequest()
                 .authenticated()
                 .and()
